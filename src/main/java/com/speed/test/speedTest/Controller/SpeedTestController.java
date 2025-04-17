84f93847173b138759421c6c7150bd35a7eeb5bd
meenabva <68288484+meenabva@users.noreply.github.com>
2025-04-18 00:32:22 +0530

    API to get download speed mbps

--- src/main/java/com/speed/test/speedTest/Controller/SpeedTestController.java
+++ src/main/java/com/speed/test/speedTest/Controller/SpeedTestController.java
@@ -1,7 +1,10 @@
 package com.speed.test.speedTest.Controller;
 
 import com.speed.test.speedTest.Service.SpeedTestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpeedTestController {
 
+    Logger log = LoggerFactory.getLogger(SpeedTestController.class);
     @Autowired
     SpeedTestService speedTestService;
 
     @GetMapping("/ping")
     public ResponseEntity<String> getPingSpeed(){
+        log.debug("REST request to get ping speed");
         return ResponseEntity.ok(speedTestService.getPingSpeed());
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Double> getDownloadSpeed(){
+        log.debug("REST request to get download speed");
+        try {
+            Double speed = speedTestService.getDownloadSpeed();
+            return ResponseEntity.ok(speed);
+        } catch (Exception e){
+            log.debug("Exception while getting download speed: {}", e.getMessage(), e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/speed/test/speedTest/Service/Impl/SpeedTestServiceImpl.java
+++ src/main/java/com/speed/test/speedTest/Service/Impl/SpeedTestServiceImpl.java
@@ -3,7 +3,9 @@ package com.speed.test.speedTest.Service.Impl;
 import com.speed.test.speedTest.Service.SpeedTestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,7 +14,11 @@ import java.io.InputStreamReader;
 public class SpeedTestServiceImpl implements SpeedTestService {
 
     Logger log = LoggerFactory.getLogger(SpeedTestServiceImpl.class);
-    private String HOST_URL = "google.com";
+    private static final String HOST_URL = "google.com";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private static final String DOWNLOAD_URL = "http://speedtest.tele2.net/10MB.zip";
 
     @Override
     public Double getUploadSpeed() {
@@ -21,11 +27,26 @@ public class SpeedTestServiceImpl implements SpeedTestService {
 
     @Override
     public Double getDownloadSpeed() {
-        return null;
+        log.debug("Request to get download speed");
+        Double speedMbps = null;
+        long startTime = System.currentTimeMillis();
+        ResponseEntity<byte[]> response = restTemplate.getForEntity(DOWNLOAD_URL, byte[].class);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        byte[] downloadData = response.getBody();
+        if (downloadData == null) {
+            throw new RuntimeException("Error getting the download speed.");
+        }
+        double fileSizeInMb = downloadData.length / (1024.0 * 1024.0);
+        double timeInSec = duration / 1000.0;
+        speedMbps = (fileSizeInMb * 8) / timeInSec;
+        log.debug("Downloaded {} MB in {} s --→ {} Mbps", fileSizeInMb, timeInSec, speedMbps);
+        return speedMbps;
     }
 
     @Override
     public String getPingSpeed() {
+        log.debug("Request to get ping speed");
         try{
             ProcessBuilder processBuilder = new ProcessBuilder("ping", "-n", "4", HOST_URL);
             Process process = processBuilder.start();
